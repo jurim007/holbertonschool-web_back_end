cabfde56d34d54460973bf6d313fe6d786851a7a
007codename <juri.malaj@gmail.com>
2026-08-18 17:08:20 +0200

    task 10: finished and ready for test correction

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  map.forEach((quantity, key) => {
+    if (quantity === 1) {
+      map.set(key, 100);
+    }
+  });
+
+  return map;
+}
